c42469a83db2f67a077902bdd5ee923d3bd29799
M-Talha183 <shahzainali5002@gmail.com>
2026-03-29 07:52:14 +0500

    Complete chapter 04

---
+++ chapter_04/app.js
@@ -0,0 +1,32 @@
+// chapter 04
+
+// answer 01
+let name , age , salarey;
+
+// answer 02
+// legal
+let NAME ;
+let myName;
+let MyName;
+let myname
+let my_name;
+let $age;
+
+// illegal
+
+// // let @ge
+// let 1name;
+// let my name;
+// let my-name;
+// let my.name;
+
+// answer 3
+document.writeln("<h1>Rules for naming JS variables</h1>");
+
+document.writeln("Variable names can only contain letters, numbers, $ and _. For example $my_1stVariable <br><br>");
+
+document.writeln("Variables must begin with a letter, $ or _. For example $name, _name or name <br><br>");
+
+document.writeln("Variable names are case sensitive <br><br>");
+
+document.writeln("Variable names should not be JS keywords");
