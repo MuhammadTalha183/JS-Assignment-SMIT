84620073b947b3a83b9a63d5123f1e7e644664eb
M-Talha183 <shahzainali5002@gmail.com>
2026-04-14 23:54:27 +0500

    Add new task

--- chapter_17-18/app.js
+++ chapter_17-18/app.js
@@ -3,3 +3,16 @@ for (let i = 1; i <= 100; i++)
     if (i % 2 === 0) {
         console.log(i);
     }
+
+while (true) {
+    let input = prompt("Enter a number (or 'exit' to quit):");  
+    if (input.toLowerCase() === 'exit') {
+        break;  
+    }   
+    let number = parseFloat(input);
+    if (isNaN(number)) {
+        console.log("Please enter a valid number.");
+    } else {
+        console.log("You entered: " + number);
+    }
+}
