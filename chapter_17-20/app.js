dbac8b393b9a393873cd8acaa2beee33dea19c8d
M-Talha183 <shahzainali5002@gmail.com>
2026-04-15 22:57:49 +0500

    add chapter 17 to 20 tasks for loops

---
+++ chapter_17-20/app.js
@@ -0,0 +1,19 @@
+// Declare and initialize an empty multidimensional array.
+// (Array of arrays)
+let multiArray = [];
+
+// Declare and initialize a multidimensional array
+// representing the following matrix:
+// 1 2 3
+// 4 5 6
+// 7 8 9
+let matrix = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9]
+];
+
+// Write a program to print numeric counting from 1 to 10.
+for (let i = 1; i <= 10; i++) {
+  console.log(i \n);
+}
