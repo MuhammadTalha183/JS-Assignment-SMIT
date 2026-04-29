559dad5293cd2694890be389d7f36575f722aaef
M-Talha183 <ullahehsan016@gmail.com>
2026-04-29 22:05:14 +0500

    Add new chapter

---
+++ CHAPTER_21-24/app.js
@@ -0,0 +1,10 @@
+//  chapter 20 to 
+// 24
+
+// 1. Write a program that takes two user inputs for first and last name using prompt and merge them in a new variable fullName. Greet the user using his full name.
+// var firstName = prompt("Enter your first name:");
+// var lastName = prompt("Enter your last name:");
+// var fullName = firstName + " " + lastName;
+// alert("Hello, " + fullName + "!");
+// 2. Write a program to take a user input about his favorite mobile phone model. Find and display the length of user input in your browser
+// var favoritePhone = prompt("Enter your favorite mobile phone model:");
