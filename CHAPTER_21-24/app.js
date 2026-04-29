2e4ded5df658cda384ca7b29418096827991d22a
M-Talha183 <ullahehsan016@gmail.com>
2026-04-29 22:06:27 +0500

    Add new chapter

--- CHAPTER_21-24/app.js
+++ CHAPTER_21-24/app.js
@@ -7,4 +7,13 @@
 // var fullName = firstName + " " + lastName;
 // alert("Hello, " + fullName + "!");
 // 2. Write a program to take a user input about his favorite mobile phone model. Find and display the length of user input in your browser
-// var favoritePhone = prompt("Enter your favorite mobile phone model:");
+// var favoritePhone = prompt("Enter your favorite mobile phone model:")
+// var inputLength = favoritePhone.length
+//  
+// alert("The length of your input is: " + inputLength);
+// 3. Write a program to find the index of letter “n” in the word “Pakistani” and display the result in your browser
+// var word = "Pakistani";
+// var indexOfN = word.indexOf("n");
+// alert("The index of letter 'n' in the word 'Pakistani' is: " + indexOfN);
+// 4. Write a program to find the last index of letter “l” in the word “Hello World” and display the result in your browser
+// var phrase = "Hello World";
